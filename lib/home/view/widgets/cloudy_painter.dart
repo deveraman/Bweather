0d12201023d065a8a40e9aa5d6d56d3c1991af07
ramanverma2k <ramanverma4183@gmail.com>
2021-12-28 10:39:32 +0530

    homepage: cloudy background canvas (2/3)

--- lib/home/view/widgets/cloudy_painter.dart
+++ lib/home/view/widgets/cloudy_painter.dart
@@ -1,18 +1,46 @@
 import 'package:bweather/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class CloudyBackground extends StatelessWidget {
+class CloudyBackground extends HookWidget {
   const CloudyBackground({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final _tickerProvider = useSingleTickerProvider();
+    final _animationController = useAnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: _tickerProvider,
+    );
+
+    final _widthDrawAnimation =
+        Tween<double>(begin: 0, end: MediaQuery.of(context).size.width).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeIn),
+    );
+
+    final _heightDrawAnimation =
+        Tween<double>(begin: 0, end: MediaQuery.of(context).size.height)
+            .animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeIn),
+    );
+
+    useAnimation(_animationController);
+
+    useEffect(
+      () {
+        _animationController.forward();
+        return _animationController.dispose;
+      },
+      [_animationController],
+    );
+
     return Scaffold(
       backgroundColor: blueberryBlue,
       body: CustomPaint(
         painter: _CloudyWaves(),
-        child: const SizedBox(
-          height: double.infinity,
-          width: double.infinity,
+        child: SizedBox(
+          height: _heightDrawAnimation.value,
+          width: _widthDrawAnimation.value,
         ),
       ),
     );
