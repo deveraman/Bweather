1beeb4af93d8af0f5d3bec15345089151745154d
ramanverma2k <ramanverma4183@gmail.com>
2022-02-16 11:32:13 +0530

    Add device preview package to test layout on different screen sizes

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,3 +1,4 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_templates/constants/theme.dart';
@@ -10,6 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       theme: MyTheme().myPrimaryTheme,
       home: MultiBlocProvider(
         providers: [
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 import 'dart:developer' as developer;
 
 import 'package:bloc/bloc.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_templates/app/app.dart';
 import 'package:flutter_templates/observer.dart';
@@ -11,7 +13,10 @@ void main() {
     () async {
       await BlocOverrides.runZoned(
         () async => runApp(
-          const MyApp(),
+          DevicePreview(
+            enabled: !kReleaseMode,
+            builder: (context) => const MyApp(),
+          ),
         ),
         blocObserver: MyAppBlocObserver(),
       );
