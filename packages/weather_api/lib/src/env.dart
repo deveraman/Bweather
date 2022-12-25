eceb0117992a5221c94311e5918ffd214f503b08
ramanverma2k <ramanverma4183@gmail.com>
2022-12-25 18:50:03 +0530

    fix[weather_api]: a critical security issue

--- packages/weather_api/lib/src/env.dart
+++ packages/weather_api/lib/src/env.dart
@@ -4,6 +4,9 @@ part 'env.g.dart';
 
 @Envied()
 abstract class Env {
-  @EnviedField(varName: 'WEATHER_API_KEY')
+  @EnviedField(
+    varName: 'WEATHER_API_KEY',
+    obfuscate: true,
+  )
   static const key = _Env.key;
 }
